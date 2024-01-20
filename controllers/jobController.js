d2d99b1bdd950c5d2d03c29cdf66c3a1fb7b0983
Rohant <agrawalrohant@gmail.com>
2024-01-20 18:23:30 +0530

    SQL connection with insert of request

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -1,11 +1,38 @@
+const short = require("short-uuid");
+const connection = require("../db");
+
 /* Handler */
-function createJobHandler(req, res) {
-  //console.log(req.userInfo);
-  res.json({
-    status: 200,
-    message: "Data found",
-    claims: req.userInfo,
-  });
+async function createJobHandler(req, res) {
+  const id = short.generate();
+  const userInfo = req.userInfo;
+  const sql = `insert into jobservice_request ( requestId, requestStatus,createdDate,userId,userName,email,appid,clientId,tenentId, audience, issueAtDate, imageBase64) 
+  values (${JSON.stringify(id)}, "REQUEST_RECEIVED", NOW(),${parseInt(
+    userInfo.userID
+  )},${JSON.stringify(userInfo.UserName)},${JSON.stringify(userInfo.email)},${
+    userInfo.appid
+  },${userInfo.clientId},${userInfo.tenentId},${JSON.stringify(
+    userInfo.audience
+  )}, ${JSON.stringify(formatForDB(userInfo.IssueAtTime))}, ${JSON.stringify(
+    req.body.content
+  )});`;
+  try {
+    //console.log("Statement : " + sql);
+    const [result, fields] = await connection.query(sql);
+    //console.log(result);
+    //console.log(fields);
+    return res.json({
+      status: 200,
+      requestId: id,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+function formatForDB(epochTimeStamp) {
+  //console.log("epochTimeStamp :" + epochTimeStamp);
+  //console.log("date :" + date.toISOString());
+  return new Date(epochTimeStamp).toISOString().slice(0, 19).replace("T", " ");
 }
 
 module.exports = { createJobHandler };
---
+++ db.js
@@ -0,0 +1,6 @@
+const mysql = require('mysql2');
+
+const pool = mysql.createPool(process.env.DB_URL);
+
+
+module.exports = pool.promise();
